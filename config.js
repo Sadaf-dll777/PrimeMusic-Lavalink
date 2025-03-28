c1cadfbd990934668082910c379ada8ee8f4b6f5
Sadaf-dll777 <md.saifalsadaf@gmail.com>
2025-03-28 14:55:47 +0600

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ module.exports = {
       name: "Avinan",
       password: "avinan",
       host: "new-york-node-1.vortexcloud.xyz",
-      port:  9323,
+      port:  5008,
       secure: false
-} 
+}
   ]
 }
